b3b645723d085eddf901beeb5f172cc8bb306999
jingjingke <919948650@qq.com>
2017-05-08 13:43:03 +0800

    优化分页

--- src/components/page-numbers.js
+++ src/components/page-numbers.js
@@ -28,8 +28,8 @@ export default class extends Component {
 				next = (<span className="next disNone">无</span>);
 			}
 			
-			//所有页码
-			var list = [];
+			//所有页码--暂存到temp
+			var temp = [];
 			var styleName;
 			for(var i = 1; i <= total; i++){
 				var thisEle;
@@ -39,7 +39,40 @@ export default class extends Component {
 					styleName='';
 				}
 				thisEle = (<span key={i} className={styleName} onClick={this.props.thisclick}>{i}</span>)
-				list.push(thisEle)
+				temp.push(thisEle)
+			}
+			//提取需要的页码--list正式
+			var list = [];
+			//判断是否需要加省略号的
+			if(total < 8){
+				//正常赋值 list=temp就可以
+				list = temp;
+			}else{
+				list.push(temp[0])
+				if(now <= 4 ){
+					//当前页偏前时的情况
+					for(let i=1; i<5; i++){
+						list.push(temp[i])
+					}
+					list.push((<span key={total+1}>···</span>))
+					//判断完
+				}else if(now >= total-3){
+					//当前页偏后时的情况
+					list.push((<span key='-1'>···</span>))
+					for(let i=total-5; i<total-1; i++ ){
+						list.push(temp[i])
+					}
+					//判断完
+				}else{
+					//当前面在中间区间的情况
+					list.push((<span key='-1'>···</span>))
+					for(let i=now-2;i<now+1;i++){
+						list.push(temp[i])
+					}
+					list.push((<span key={total+1}>···</span>))
+					//判断完
+				}
+				list.push(temp[total-1])
 			}
 			
 			//渲染
